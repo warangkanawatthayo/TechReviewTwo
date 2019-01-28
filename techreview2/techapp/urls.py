65d684e9fa6d72ed9cf13841972bf95b2c3ce04c
Nice Watthayo <watthayo.warangkana@seattlecentral.edu>
2019-01-28 11:55:02 -0800

    added resource view

--- techreview2/techapp/urls.py
+++ techreview2/techapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('techtype/', views.techtype, name='types')
 ]
--- techreview2/techapp/views.py
+++ techreview2/techapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from.models import ProductType
 
 # Create your views here.
 def index(request):
     return render(request,'techapp/index.html')
+
+def techtype (request):
+    type_list=ProductType.objects.all()
+    return render (request, 'techapp/types.html', {'type_list': type_list})
