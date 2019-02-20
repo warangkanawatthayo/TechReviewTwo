0eac3d0946fe4cef65672ac5465ea9af0c77faba
Nice Watthayo <watthayo.warangkana@seattlecentral.edu>
2019-02-20 12:02:33 -0800

    add forms

--- techreview2/techapp/urls.py
+++ techreview2/techapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     path('techtypes/', views.techtypes, name='techtypes'),
     path('getproducts/', views.getproducts, name='getproducts'),
     path('productdetail/<int:id>', views.productdetail, name='details'),
+    path('newProduct/', views.newProduct, name='newproduct'),
 ]
--- techreview2/techapp/views.py
+++ techreview2/techapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import ProductType, Product
+from .forms import ProductForm
 
 # Create your views here.
 def index (request):
@@ -7,7 +8,7 @@ def index (request):
 
 def techtypes (request):
     type_list=ProductType.objects.all()
-    return render (request, 'techapp/types.html', {'type_list': type_list})
+    return render(request, 'techapp/types.html', {'type_list': type_list})
 
 def getproducts(request):
     product_list=Product.objects.all()
@@ -17,3 +18,16 @@ def productdetail(request, id):
     detail=get_object_or_404(Product, pk=id)
     context = { 'detail': detail}
     return render (request, 'techapp/details.html',context=context )
+#form view
+def newProduct(request):
+    form=ProductForm
+    if request.method=='POST':
+        form=ProductForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=True)
+            post.save()
+            form=ProductForm()
+    else:
+        form=ProductForm()
+    return render(request, 'techapp/newproduct.html', {'form': form})
+
